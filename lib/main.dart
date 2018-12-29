739ce83af69c2a703ef4387957bd931530d6ca99
Jason Franklin <jason.franklin@xing.com>
2018-12-29 10:10:11 +0100

    Add text input field and send button

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,54 @@ class FriendlyChat extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State {
+  final _textEditingController = TextEditingController();
+
+  void _onSubmit(String text) {
+    _textEditingController.clear();
+  }
+
+  Widget _buildTextFieldContainer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textEditingController,
+                onSubmitted: _onSubmit,
+                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _onSubmit(_textEditingController.text),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Friendly Chat"),
         ),
-        body: Center(child: Text("body"))
+        body: _buildTextFieldContainer()
     );
   }
 }
