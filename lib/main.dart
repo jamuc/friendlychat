6392d846ca95937ff95cb45017a77072937557f8
Jason Franklin <jason.franklin@xing.com>
2018-12-30 09:44:52 +0100

    Add messages to the chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const String _name = "Your name";
+
 main(List<String> args) {
   runApp(FriendlyChat());
 }
@@ -15,6 +17,37 @@ class FriendlyChat extends StatelessWidget {
   }
 }
 
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
+
 class ChatScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -23,10 +56,15 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State {
+  final _messages = <ChatMessage>[];
   final _textEditingController = TextEditingController();
 
   void _onSubmit(String text) {
     _textEditingController.clear();
+    final _message = new ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, _message);
+    });
   }
 
   Widget _buildTextFieldContainer() {
@@ -62,7 +100,23 @@ class ChatScreenState extends State {
         appBar: AppBar(
           title: Text("Friendly Chat"),
         ),
-        body: _buildTextFieldContainer()
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextFieldContainer()
+            )
+          ],
+        )
     );
   }
 }
